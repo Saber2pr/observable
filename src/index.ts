9db1438b9947fd4a7818a66214727ccf1141dbd3
saber2pr <saber2pr@gmail.com>
2019-01-04 16:28:45 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 export * from './core/Observable'
+
+import { Observable } from './core/Observable'
+
+new Observable({ value: 100 })
+  .pipe(state => ({ value: state.value + 233 }))
+  .subscribe(state => console.log(state))
